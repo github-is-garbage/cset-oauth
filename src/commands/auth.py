e9f921c146f1032dc8c8d102ea5f54d37371a364
github-is-garbage <github-is-garbage@users.noreply.github.com>
2024-09-18 01:54:52 -0400

    Test MSAL authentication code

--- src/commands/auth.py
+++ src/commands/auth.py
@@ -1,6 +1,27 @@
 from bot import Bot
 import discord
+import msal
+import os
 
 @Bot.tree.command(name = "auth")
 async def auth(interaction: discord.Interaction):
-	await interaction.response.send_message("Response", ephemeral = True)
+	await interaction.response.send_message("Sending request to Microsoft...", ephemeral = True)
+
+	App = msal.PublicClientApplication(os.getenv("AZURE_APPLICATION_ID"), authority = os.getenv("MSAL_AUTHORITY"))
+	Flow = App.initiate_device_flow(scopes = [ os.getenv("MSAL_SCOPE") ])
+
+	UserCode = Flow.get("user_code")
+	VerificationURL = Flow.get("verification_uri")
+
+	if UserCode is None or VerificationURL is None:
+		await interaction.edit_original_response(content = "Failed to generate a linking code.\nPlease contact an administrator.")
+
+	await interaction.edit_original_response(content = f"To sign in, use a web browser to open the page <{VerificationURL}> and enter the code `{UserCode}`")
+
+	Result = App.acquire_token_by_device_flow(Flow)
+	AccessToken = Result.get("access_token")
+
+	if AccessToken is None:
+		await interaction.edit_original_response(content = "Failed to link account.\nPlease try again.")
+
+	await interaction.edit_original_response(content = "Something happened")
