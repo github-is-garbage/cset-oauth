cbb65402ee3ec29af67f11a23023556c80ef1c30
github-is-garbage <github-is-garbage@users.noreply.github.com>
2024-11-04 20:28:17 -0500

    Use Canvas API to get basic user info

--- src/commands/auth.py
+++ src/commands/auth.py
@@ -2,7 +2,14 @@ from bot import Bot
 from threadpool import RunInThread
 import discord
 import logging
+from canvasapi import Canvas, current_user
 import os
+
+async def GetUserProfile(CanvasGateway: Canvas):
+	CanvasUser: current_user.CurrentUser = CanvasGateway.get_current_user()
+	CanvasProfile = CanvasUser.get_profile() # If the token is invalid this will error
+
+	return CanvasProfile
 
 @Bot.tree.command(name = "auth")
 @discord.app_commands.describe(access_token = "Your Canvas API Access Token. Run the /help command for information on obtaining one.")
@@ -12,13 +19,15 @@ async def auth(Interaction: discord.Interaction, access_token: str):
 	await Interaction.response.send_message("Starting linking process...", ephemeral = True)
 
 	try:
-		await Interaction.edit_original_response(content = "d")
+		CanvasGateway = Canvas(os.environ.get("CANVAS_API_URL"), access_token)
+		CanvasProfile = await GetUserProfile(CanvasGateway)
+
+		UserName = CanvasProfile.get("name")
+		if UserName is None:
+			return await Interaction.edit_original_response(content = "This account does not have a valid name") # Should never happen
+
+		await Interaction.edit_original_response(content = f"Linking as `{ CanvasProfile.get("name") }`")
 	except Exception as Error:
 		logging.getLogger("discord.client").error(f"Link request {Interaction.user.id} had something go very wrong!\n{Error}")
 
-		try:
-			# This may not be the exact reason, but we need something to tell them
-			#await Bail(Interaction, "Your request has expired.\nPlease try again.")
-			pass
-		except:
-			logging.getLogger("discord.client").error(f"Failed to notify {Interaction.user.id} of error")
+		await Interaction.edit_original_response(content = "Something went wrong :(")
