a5fcbafc8f5d395fc2ed814dd569d99d6d952a4d
github-is-garbage <github-is-garbage@users.noreply.github.com>
2024-11-05 15:36:42 -0500

    Use SQLAlchemy URL object

--- src/db.py
+++ src/db.py
@@ -1,7 +1,16 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, URL
 import os
 
-# .env is terrible with all the getenv's
-ConnectionURL = f"mysql+mysqlconnector://{ os.getenv("MYSQL_USERNAME") }:{ os.getenv("MYSQL_PASSWORD") }@{ os.getenv("MYSQL_URL") }:{ os.getenv("MYSQL_PORT") }/{ os.getenv("MYSQL_DATABASE") }"
+ConnectionURL = URL.create(
+	"mysql+mysqlconnector",
+
+	os.getenv("MYSQL_USERNAME"),
+	os.getenv("MYSQL_PASSWORD"),
+
+	os.getenv("MYSQL_URL"),
+	os.getenv("MYSQL_PORT"),
+
+	os.getenv("MYSQL_DATABASE")
+)
 
 Engine = create_engine(ConnectionURL, echo = True)
